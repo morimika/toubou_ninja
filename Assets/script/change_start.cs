9709598919f7bbc308332b1322ac1a9415ca7d34
morimika <ttc22c1020048@edu.tech.ac.jp>
2022-10-18 16:04:10 +0900

    221008_02_mori_button

---
+++ Assets/script/change_start.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class change_start : MonoBehaviour
+{
+    //‰æ–Ê‘JˆÚ
+    public void OnClick()
+        {
+            SceneManager.LoadScene("Game");
+        }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

1fbbfcab127c70c01c543037d07cac0621c6123e
morimika <ttc22c1020048@edu.tech.ac.jp>
2022-10-20 15:40:05 +0900

    221020_03_waterrun_chara

---
+++ Assets/script/CharaJump.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaJump : MonoBehaviour
+{
+    private Rigidbody2D rbody2D;
+
+    private float jumpForce = 10f;
+
+    private int jumpCount = 0;
+
+    private float speed = 0.01f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = transform.position;
+
+        if(Input.GetKey(KeyCode.Space)/*&&this.jumpCount<1*/)
+        {
+            this.rbody2D.AddForce(transform.up * jumpForce);
+            //jumpCount++;
+            position.x += speed;
+        }
+        transform.position = position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Floor"))
+        {
+            jumpCount = 0;
+        }
+    }
+}
